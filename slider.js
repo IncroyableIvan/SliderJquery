0138e261f13532ed78a59f63551cddcc12733a56
Ivan Zanazzi <ivan.zanazzi@gmail.com>
2017-06-28 09:12:58 +0000

    Loading of imgs and destruction of HTML hook

---
+++ slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  initializeSlider();
+
+  function initializeSlider() {
+    // Load the HTML hook
+    var sliderData = $('#slider-container img');
+    // If the hook is broken; throw error to stop script
+    if(sliderData.length == 0) {
+      throw "Erreur de selection";
+    }
+    $('#slider-container').remove();
+  }
+});
