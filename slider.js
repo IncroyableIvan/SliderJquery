0212977671acbf4a86080c9cd60d7fd27aa93024
Ivan Zanazzi <ivan.zanazzi@gmail.com>
2017-06-28 15:26:20 +0000

    realese of v1

--- slider.js
+++ slider.js
@@ -7,6 +7,29 @@ $(document).ready(function() {
   createSlider();
   render();
 
+  function left() {
+    var previousImage = $('#selected').prev()[0];
+    if(previousImage == undefined) {
+      var previousImage = $('#slider-container div').last()[0];
+    }
+    $('#selected').css('display', 'none');
+    $('#selected').removeAttr('id');
+
+    $(previousImage).css('display', 'flex').css('justify-content', 'center');
+    $(previousImage).attr('id', 'selected');
+  }
+  function right() {
+    var nextImage = $('#selected').next()[0];
+    if(nextImage == undefined) {
+      var nextImage = $('#slider-container div').first()[0];
+    }
+
+    $('#selected').css('display', 'none');
+    $('#selected').removeAttr('id');
+
+    $(nextImage).css('display', 'flex').css('justify-content', 'center');
+    $(nextImage).attr('id', 'selected');
+  }
   function initializeSlider() {
     // Load the HTML hook
     sliderData = $('#slider-container img');
@@ -21,20 +44,28 @@ $(document).ready(function() {
     $('body').append(sliderHTML);
   }
   function createSlider () {
-    sliderHTML += "<div id='slider-container' style='display:flex;height=500px;'>";
+    sliderHTML += "<div id='slider-container' style='display:flex;flex-direction:row;overflow:hidden;background-color:black;height:" + height + "px'>"
     for (var i = 0; i < sliderData.length; i++) {
       var display = "";
+      var id = "";
       if(i == 0) {
         display = "display:flex;justify-content:center;";
+        id = "selected";
       } else {
         display = "display:none;";
       }
-      sliderHTML += "<div style='width:100%;height:500px;" + display +"'>";
+      sliderHTML += "<div id= '" + id + "' style='width:100%;height:500px;" + display +"'>";
       sliderHTML += "<img src='" + $(sliderData[i])[0].src + "' style='height:100%;'>";
       sliderHTML += "</div>";
     }
     sliderHTML += "</div>";
-    sliderHTML += "<button data-action='left'><</button>";
-    sliderHTML += "<button data-action='right'>></button>";
+
+    sliderHTML += "<div id='button' style='display:flex;justify-content:space-between;position:fixed;top:" + (height - 64)/2 + "px;width:99%'>"
+    sliderHTML += "<button style='color:white;background-color:transparent;border:none;font-size:50px;' data-action='left'><</button>";
+    sliderHTML += "<button style='color:white;background-color:transparent;border:none;font-size:50px;' data-action='right'>></button>";
+    sliderHTML += "</div>"
+
+    $('body').on('click', "button[data-action='left']", left);
+    $('body').on('click', "button[data-action='right']", right);
   }
 });
